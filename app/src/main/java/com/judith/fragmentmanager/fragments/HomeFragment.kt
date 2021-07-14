fb8c9e68a38746e35de2b801c5c6207eb444c18f
IambabeJ <afamudojudy@gmail.com>
2021-07-14 14:33:16 +0100

    initial commit

---
+++ app/src/main/java/com/judith/fragmentmanager/fragments/HomeFragment.kt
@@ -0,0 +1,23 @@
+package com.judith.fragmentmanager.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.judith.fragmentmanager.R
+import com.judith.fragmentmanager.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding= FragmentHomeBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+}
